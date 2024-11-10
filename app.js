494ab7f73c89d8083a5977407ce9dbb7ffa055a4
Ashley Chung - Fah <ashliichung19@gmail.com>
2024-11-10 10:50:35 -0500

    Changes made

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 document.getElementById("search-btn").addEventListener("click", function() {
     // Create a new XMLHttpRequest object
     const xhr = new XMLHttpRequest();
-
+  
     // Define what happens when the response is loaded
     xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Show the list of superheroes as an alert
-            alert(xhr.responseText);
-        } else {
-            alert("An error occurred while trying to fetch the data.");
-        }
+      if (xhr.status === 200) {
+        // Show the list of superheroes as an alert
+        alert(xhr.responseText);
+      } else {
+        alert("An error occurred while trying to fetch the data.");
+      }
     };
-
+  
     // Configure the AJAX request to superheroes.php
-    xhr.open("GET", "superheroes.php", true);
-
+    xhr.open("GET", "superheroes.php", true); // Set fourth argument to true
+  
     // Send the request
     xhr.send();
-});
+  });
